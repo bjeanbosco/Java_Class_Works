5289d63cfb6cfa680b57828d76ba7e9aa3b816a4
Jean Bosco <jeanboscob48@gmail.com>
2023-11-02 20:26:27 +0200

    These files codes about how to use for loop as control statement in java

---
+++ forLoop.java
@@ -0,0 +1,8 @@
+class Gikondo {
+    public static void main(String args[]) {
+        byte score;
+        for (score = 50; score < 100; score++) {
+            System.out.println("You have " + score + " scores Big Congratulations.");
+        }
+    }
+}
