2555b58caca44cefb1cacf152a0f6d67caf8f12d
Jean Bosco <jeanboscob48@gmail.com>
2023-11-02 20:57:47 +0200

    this is about ternary operator

---
+++ ternaryOperatorsecond.java
@@ -0,0 +1,8 @@
+public class ternaryOperatorsecond {
+    public static void main(String args[]) {
+        int a = 10;// variable
+        int b = 5;// varaible
+        int min = (a < b) ? a : b;// ternary operator ? work as if and : works as else
+        System.out.println(min);
+    }
+}
