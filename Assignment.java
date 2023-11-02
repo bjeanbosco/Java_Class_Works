7a24886d3951a9c468358393c187efc0c2cc090c
Jean Bosco <jeanboscob48@gmail.com>
2023-11-02 20:11:28 +0200

    this shows how to use assignment operators in short hand according to senario

---
+++ Assignment.java
@@ -0,0 +1,10 @@
+public class Assignment {
+    public static void main(String args[]) {
+        int a = 10;// this is variable a that store interger data type value
+        int b = 20;// this is variable b that store interger data type value
+        a += 4;// a=a+4 (a=10+4)
+        b -= 4;// b=b-4 (b=20-4)
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
