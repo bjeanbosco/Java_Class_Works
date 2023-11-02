9365153c4a77754ae55deaae04990cc98458f410
Jean Bosco <jeanboscob48@gmail.com>
2023-11-02 20:12:47 +0200

    this also show to use assignment in short hand a+=b

---
+++ Assignment2.java
@@ -0,0 +1,13 @@
+public class Assignment2 {
+    public static void main(String[] args) {
+        int a = 10; // this is variable a that store interger data type value
+        a += 3;// 10+3
+        System.out.println(a);
+        a -= 4;// 13-4
+        System.out.println(a);
+        a *= 2;// 9*2
+        System.out.println(a);
+        a /= 2;// 18/2
+        System.out.println(a);
+    }
+}
