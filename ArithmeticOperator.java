30b68ca15e6e768b6734e38a7da3f851df80927b
Jean Bosco <jeanboscob48@gmail.com>
2023-11-02 20:07:34 +0200

    This files contains the codes about different arthmetic operators in different senario

---
+++ ArithmeticOperator.java
@@ -0,0 +1,11 @@
+public class ArithmeticOperator {
+    public static void main(String args[]) {
+        int a = 10; // this is variable a that store interger value in memory
+        int b = 5;// this is variable b that store interger value in memory
+        System.out.println(a + b);// 15
+        System.out.println(a - b);// 5
+        System.out.println(a * b);// 50
+        System.out.println(a / b);// 2
+        System.out.println(a % b);// 0
+    }
+}
