cf2cbdf39689faf6f03b8e9a7b14dcb034e26ba1
Jean Bosco <jeanboscob48@gmail.com>
2023-11-02 20:09:52 +0200

    this files contains codes that shows how to add the two numbers of short data type in java

---
+++ assignemet4.java
@@ -0,0 +1,8 @@
+public class assignemet4 {
+    public static void main(String args[]) {
+        short a = 10; // this is variable a that store short data type value
+        short b = 10;// this is variable b that store short data type value
+        a = (short) (a + b);// 20 which is int now converted to short
+        System.out.println(a); // 20
+    }
+}
