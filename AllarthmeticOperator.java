90eade733408edc2c5d0d1b35a8cd33fba3213bc
Jean Bosco <jeanboscob48@gmail.com>
2023-11-02 20:04:11 +0200

    this file contains codes that combine all arthmetic operators in one example

---
+++ AllarthmeticOperator.java
@@ -0,0 +1,7 @@
+public class AllarthmeticOperator {
+
+    public static void main(String args[]) {// this is main method or function where compiler start compilation process
+        System.out.println(10 * 10 / 5 + 3 - 1 * 4 / 2);// this print number calculated from this equation
+        // all arthimetic aperator used at the same time
+    }
+}
