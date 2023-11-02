e912fc77e0bf856bc83b47be263e563c5771f9cc
Jean Bosco <jeanboscob48@gmail.com>
2023-11-02 21:00:55 +0200

    this is how to use carot operator and to use logic not operator as unary operator

---
+++ UnaryOperatorSecond.java
@@ -0,0 +1,12 @@
+public class UnaryOperatorSecond {
+    public static void main(String args[]) {
+        int a = 10;// integer variable
+        int b = -10;// integer variable
+        boolean c = true;// boolean variable
+        boolean d = false;// variable
+        System.out.println(~a);// -11 (minus of total positive value which starts from 0)
+        System.out.println(~b);// 9 (positive of total minus, positive starts from 0)
+        System.out.println(!c);// false (opposite of boolean value)
+        System.out.println(!d);// true
+    }
+}
