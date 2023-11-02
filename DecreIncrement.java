1cb581100925fcbc43524eba551c96749d016f0b
Jean Bosco <jeanboscob48@gmail.com>
2023-11-02 20:18:37 +0200

    these files show how to use unary operator of decrement

---
+++ DecreIncrement.java
@@ -0,0 +1,10 @@
+public class DecreIncrement {
+
+    public static void main(String args[]) {
+        int x = 10;// integer data type
+        System.out.println(x++);// 10 (11)
+        System.out.println(++x);// 12
+        System.out.println(x--);// 12 (11)
+        System.out.println(--x);// 10
+    }
+}
