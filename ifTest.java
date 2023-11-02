f3eb39882b3ae05c17d2acd6f42b8e38fbb9b019
Jean Bosco <jeanboscob48@gmail.com>
2023-11-02 20:29:44 +0200

    this is sample of example of using if to check whether condition is true

---
+++ ifTest.java
@@ -0,0 +1,8 @@
+class IfTest {
+    static public void main(String args[]) {
+        int salary = 100000;
+        if (salary >= 100000) {
+            System.out.println("You are alloed Bonus");
+        }
+    }
+}
