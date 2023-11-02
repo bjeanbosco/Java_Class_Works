d91f51ff902434c34cf9425f63348adfc94a9488
Jean Bosco <jeanboscob48@gmail.com>
2023-11-02 20:28:10 +0200

    this files show how to use if and else as one way of making decision

---
+++ ifElse.java
@@ -0,0 +1,11 @@
+class ifElse {
+    public static void main(String args[]) {
+        int age = 15;
+        if (age >= 18) {
+            System.out.println("You are to apply for Scholarship.");
+        } else {
+            System.out.println("You are young and not allowed to apply for Scholarship.");
+        }
+    }
+
+}
